623782714138083c0d0cee6794f7cecc0e9dcbc1
knowsWhereHisTowelIs <cscalebslater@gmail.com>
2017-10-24 14:30:53 -0400

    Added TODO list

--- three_axis_gimbal/config.m
+++ three_axis_gimbal/config.m
@@ -1,7 +1,26 @@
 %---------------------------------------------------
 %               Starting Params
 %---------------------------------------------------
+set_param();
 
+%{
+Function Gen
+    50 hz
+    2.2 peak to peak
+    1.2 offset
+    clockwise 5% - 7.5%
+    counterclockwise 7.5% to 10%
+Power
+    11.6 volts
+    1.5 ampsish?
+    
+Motor P (Camera holder)
+    White wires
+Motor R (B4 Camera)
+    Red wires
+Motor Y (Base)
+    Blue wires
+%}
 %---------------------------------------------------
 %               More?
 %---------------------------------------------------
